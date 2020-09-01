12302a1b1a02ecb81fe53e40f9bce7fa9109e519
Corentin-Sueur <corentin.sueur@outlook.com>
2020-09-01 14:36:31 +0200

    rangement du routing au bon endroit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { PageContactComponent } from './page-contact/page-contact.component';
+import { PageEtudiantsComponent } from './page-etudiants/page-etudiants.component';
+import { PageNousDecouvrirComponent } from './page-nous-decouvrir/page-nous-decouvrir.component'
+import { FourOFourComponent } from './four-o-four/four-o-four.component';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'contact', component: PageContactComponent },
+  { path: 'etudiant', component: PageEtudiantsComponent },
+  { path: 'nous-decouvrir', component: PageNousDecouvrirComponent },
+  { path: '', component: AccueilComponent },
+  { path: '404', component: FourOFourComponent },
+  { path: '**', redirectTo: '/404' }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
